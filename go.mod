0d8349cf52c8160c1ecd61b3951ea4b9b75f06e2
Jeiel <jeiel.moura.silva@gmail.com>
2023-01-29 17:42:57 -0300

    partial commit: env config

--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.2.1 // indirect
 	github.com/googleapis/gax-go/v2 v2.7.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/net v0.5.0 // indirect
 	golang.org/x/oauth2 v0.4.0 // indirect
--- src/config/env.go
+++ src/config/env.go
@@ -1,8 +1,12 @@
 package config
 
 import (
+	"log"
+	"os"
 	"path/filepath"
 	"runtime"
+
+	"github.com/joho/godotenv"
 )
 
 func getEnvsPath() string {
@@ -13,4 +17,24 @@ func getEnvsPath() string {
 	envsPath := filepath.Join(rootPath, "envs")
 
 	return envsPath
+}
+
+func LoadEnv() {
+	envsPath := getEnvsPath()
+
+	currentFile := "current.env"
+	currentEnvPath := filepath.Join(envsPath, currentFile)
+	err := godotenv.Load(currentEnvPath)
+	if err != nil {
+		log.Fatalf("Error loading " + currentFile + " file")
+	}
+
+	envName := os.Getenv("ENV")
+	envPath := filepath.Join(envsPath, envName)
+
+	firebasePath := filepath.Join(envPath, "firebase.json")
+	err = os.Setenv("FIREBASE_PATH", firebasePath)
+	if err != nil {
+		log.Fatalf("Error setting firebase path file")
+	}
 }
